5fc8cb6e7de1b661382f4cb75382cf36b26c3193
Oleg Telegin <teleginos@mail.ru>
2023-05-23 21:24:20 +0300

    add solution task38

--- Task38/Program.cs
+++ Task38/Program.cs
@@ -3,23 +3,16 @@
     int[] arr = new int[num];
     for(int i = 0; i < num; i++)
     {
-        arr[i] = new Random().Next(-100, 100);
+        arr[i] = new Random().Next(-10, 10);
     }
     return arr;
 }
 
-int SumOfOdd(int[] array)
+int DifferenceMaxAndMin(int[] array)
 {
-    int SumCount = 0;
-    for(int i = 1; i < array.Length; i += 2)
-    {
-        SumCount += array[i];
-    }
-    return SumCount;
+    return array.Max() - array.Min();
 }
 
-
- 
 Console.WriteLine("Введите размер массива: ");
 string number = Console.ReadLine();
 if(int.TryParse(number, out int num))
@@ -27,7 +20,7 @@ if(int.TryParse(number, out int num))
     int[] array = GenerationArray(num);
 
     Console.WriteLine($"Массив = [{String.Join(", ", array)}]\n");
-    Console.WriteLine($"Сумма элементов, стоящих на нечётных позициях = {SumOfOdd(array)}");
+    Console.WriteLine($"Разницу между максимальным и минимальным элементов массива = {DifferenceMaxAndMin(array)}");
 }
 else
 {
